5b715f3a654e9662f759f25c3f582d35be3e2e8a
Anderson Gabriel Ferreira <anderson.gabriel.ferreira@gmail.com>
2023-03-14 03:15:09 -0300

    Adiciona primeira consulta

--- app/src/main/java/br/com/gabrielorander/countrylist/data/repository/ApolloClient.kt
+++ app/src/main/java/br/com/gabrielorander/countrylist/data/repository/ApolloClient.kt
@@ -1,14 +1,29 @@
 package br.com.gabrielorander.countrylist.data.repository
 
+import br.com.gabrielorander.countrylist.CountryListQuery
+import br.com.gabrielorander.countrylist.data.mapper.toSimpleCountry
 import br.com.gabrielorander.countrylist.data.model.DetailedCountry
 import br.com.gabrielorander.countrylist.data.model.SimpleCountry
 import br.com.gabrielorander.countrylist.domain.repository.CountryRepository
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.exception.ApolloException
 
-class ApolloClient(
+class ApolloCountryClient(
     private val apolloClient: ApolloClient
 ): CountryRepository {
     override suspend fun countryList(): List<SimpleCountry> {
-        TODO("Not yet implemented")
+        val response = try{
+            apolloClient
+                .query(CountryListQuery()).execute()
+        } catch(e: ApolloException) {
+            throw CountryRepositoryException("Failed to retrieve Countries", e)
+        }
+
+        if (response.hasErrors()) {
+            throw CountryRepositoryException("Failed to retrieve country list: ${response.errors}")
+        }
+
+        return response.data?.countries?.map { it.toSimpleCountry() } ?: emptyList()
     }
 
     override suspend fun countryDetail(countryCode: String): DetailedCountry {
---
+++ app/src/main/java/br/com/gabrielorander/countrylist/data/repository/CountryRepositoryException.kt
@@ -0,0 +1,6 @@
+package br.com.gabrielorander.countrylist.data.repository
+
+class CountryRepositoryException(
+    message: String,
+    cause: Throwable? = null
+): java.lang.RuntimeException(message, cause)
