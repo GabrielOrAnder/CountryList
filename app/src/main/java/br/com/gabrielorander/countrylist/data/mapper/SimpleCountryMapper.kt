402610f2dfb25475b3719670de44e7ef766b42a2
Anderson Gabriel Ferreira <anderson.gabriel.ferreira@gmail.com>
2023-03-14 03:14:52 -0300

    Adiciona mapper para lista de países

--- app/src/main/java/br/com/gabrielorander/countrylist/data/mapper/SimpleCountryMapper.kt
+++ app/src/main/java/br/com/gabrielorander/countrylist/data/mapper/SimpleCountryMapper.kt
@@ -1,2 +1,13 @@
 package br.com.gabrielorander.countrylist.data.mapper
 
+import br.com.gabrielorander.countrylist.CountryListQuery
+import br.com.gabrielorander.countrylist.data.model.SimpleCountry
+
+fun CountryListQuery.Country.toSimpleCountry(): SimpleCountry {
+    return SimpleCountry(
+        code = code,
+        name = name,
+        emoji = emoji,
+        capital = capital ?: "No capital"
+    )
+}
